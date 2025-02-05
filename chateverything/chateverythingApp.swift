1e0908b516b8b85070492ed73a54998902bdb6bd
ltaoo <litaowork@aliyun.com>
2025-02-05 16:03:59 +0800

    Initial Commit

---
+++ chateverything/chateverythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  chateverythingApp.swift
+//  chateverything
+//
+//  Created by litao on 2025/2/5.
+//
+
+import SwiftUI
+
+@main
+struct chateverythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chateverythingTests/chateverythingTests.swift
@@ -0,0 +1,17 @@
+//
+//  chateverythingTests.swift
+//  chateverythingTests
+//
+//  Created by litao on 2025/2/5.
+//
+
+import Testing
+@testable import chateverything
+
+struct chateverythingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
